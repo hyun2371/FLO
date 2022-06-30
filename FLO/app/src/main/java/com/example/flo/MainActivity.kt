71433a5657398a1e952ad7b78e675cc3e57fd4e5
hyun2371 <loveanna1228@gmail.com>
2022-06-30 18:47:57 +0900

    week4

--- FLO/app/src/main/java/com/example/flo/MainActivity.kt
+++ FLO/app/src/main/java/com/example/flo/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.flo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.flo.databinding.ActivityMainBinding
 
 
@@ -13,18 +12,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FLO) //로딩동안 스플래시 화면
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initBottomNavigation()
 
-        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString()) //title, singer
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString(),0,60,false)
         //바인딩으로 xml의 아이디 가져옴
 
         binding.mainPlayerCl.setOnClickListener { //미니플레이어 클릭시 액티비티 전환
             val intent = Intent(this, SongActivity::class.java) //어디서 어디로 가는지
             intent.putExtra("title", song.title) //SongActivity로 택배상자 전달
             intent.putExtra("singer",song.singer)
+            intent.putExtra("second",song.second)
+            intent.putExtra("playTime",song.playTime)
+            intent.putExtra("isPlaying",song.isPlaying)
             startActivity(intent) //액티비티 전환
         }
 
@@ -37,7 +40,7 @@ class MainActivity : AppCompatActivity() {
             .commitAllowingStateLoss()
 
         binding.mainBnv.setOnItemSelectedListener{ item ->
-            when (item.itemId) { //각각의 id를 클릭했을 때 해당 프래그먼트로 전환
+            when (item.itemId) {
 
                 R.id.homeFragment -> {
                     supportFragmentManager.beginTransaction()
--- FLO/app/src/main/java/com/example/flo/SongActivity.kt
+++ FLO/app/src/main/java/com/example/flo/SongActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flo
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySongBinding
@@ -12,6 +13,8 @@ class SongActivity : AppCompatActivity() {
 
     //나중에 초기화
     lateinit var binding : ActivitySongBinding
+    lateinit var song : Song
+    lateinit var timer: Timer
     //변수 선언 방법 var test1 : String = "DD"
 
     //액티비티가 실행될 때 최초로 실행되는 함수
@@ -20,33 +23,113 @@ class SongActivity : AppCompatActivity() {
         binding = ActivitySongBinding.inflate(layoutInflater) //xml 레이아웃 메모리에 객체화
         setContentView(binding.root) //activity_song.xml 레이아웃 요소들을 마음대로 씀
 
-        if(intent.hasExtra("title") && intent.hasExtra("singer")){ //intent에 해당 키값 존재하는지
-            binding.songMusicTitleTv.text = intent.getStringExtra("title") //받아온값 담음
-            binding.songSingerNameTv.text = intent.getStringExtra("singer")
-        }
+        initSong()
+        setPlayer(song)
 
         binding.songDownIb.setOnClickListener {
             finish()
         }
 
         binding.songMiniplayerIv.setOnClickListener {
-            setPlayerStatus(false)
-        }
-
-        binding.songPauseIv.setOnClickListener {
             setPlayerStatus(true)
         }
 
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.interrupt()
+    }
+
+    private fun initSong(){
+        if(intent.hasExtra("title") && intent.hasExtra("singer")){ //intent에 해당 키값 존재하는지
+            song = Song( //putExtra
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("singer")!!,
+                intent.getIntExtra("second",0),
+                intent.getIntExtra("playTime",0),
+                intent.getBooleanExtra("isPlaying",false)
+            )
+        }
+        startTimer()
+    }
+
+    private fun setPlayer(song: Song){
+        binding.songMusicTitleTv.text = intent.getStringExtra("title")!!
+        binding.songSingerNameTv.text = intent.getStringExtra("singer")!!
+        binding.songStartTimeTv.text = String.format("%02d:%02d",song.second / 60, song.second % 60)
+        binding.songEndTimeTv.text = String.format("%02d:%02d",song.playTime / 60, song.playTime % 60)
+        binding.songProgressSb.progress = (song.second * 1000 / song.playTime)
+
+        setPlayerStatus(song.isPlaying)
 
     }
 
     fun setPlayerStatus (isPlaying : Boolean){ //정지, 재생 버튼 이미지 설정
+
+        //초기화
+        song.isPlaying = isPlaying
+        timer.isPlaying = isPlaying
+
         if(isPlaying){
-            binding.songMiniplayerIv.visibility = View.VISIBLE
-            binding.songPauseIv.visibility = View.GONE
-        } else {
-            binding.songMiniplayerIv.visibility = View.GONE //아이디값
+            binding.songMiniplayerIv.visibility = View.GONE
             binding.songPauseIv.visibility = View.VISIBLE
+        } else {
+            binding.songMiniplayerIv.visibility = View.VISIBLE//아이디값
+            binding.songPauseIv.visibility = View.GONE
+        }
+    }
+
+
+
+    private fun startTimer(){
+        timer = Timer(song.playTime,song.isPlaying)
+        timer.start()
+    }
+
+    //내부 클래스
+    inner class Timer(private val playTime: Int,var isPlaying: Boolean = true):Thread(){
+
+        private var second : Int = 0
+        private var mills: Float = 0f
+
+        override fun run() {
+            super.run()
+            try {
+                while (true){
+
+                    if (second >= playTime){
+                        break
+                    }
+
+                    if (isPlaying){
+                        sleep(50)
+                        mills += 50
+
+                        runOnUiThread {
+                            binding.songProgressSb.progress = ((mills / playTime)*100).toInt()
+                        }
+
+                        if (mills % 1000 == 0f){ //ms 1000당 s 1증가
+                            runOnUiThread {
+                                binding.songStartTimeTv.text = String.format("%02d:%02d",second / 60, second % 60)
+                            }
+                            second++
+                        }
+
+                    }
+
+                }
+
+            }catch (e: InterruptedException){
+                Log.d("Song","쓰레드가 죽었습니다. ${e.message}")
+            }
+
+
         }
     }
 }
